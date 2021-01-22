cf19c19711d1fef9d3d10f10eb26e6c47fc15b6d
a-taeyeon <taeyeon0107@naver.com>
2021-01-22 17:28:02 +0900

    taeyeon브랜치 test

--- app/src/main/java/com/bluelay/damda/MainActivity.kt
+++ app/src/main/java/com/bluelay/damda/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //taeyeon 브랜치 만들어서 push 해보겠음
     }
 }
