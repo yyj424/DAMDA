9aa31bda8b79aee1df24ee966c79107118b31423
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-01-28 21:39:59 +0900

    bucketList&DB

--- app/src/main/java/com/bluelay/damda/BucketActivity.kt
+++ app/src/main/java/com/bluelay/damda/BucketActivity.kt
@@ -1,15 +1,38 @@
 package com.bluelay.damda
 
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class BucketActivity : AppCompatActivity() {
-    data class DtoBucket(var content: String, var checked: Int)
+    var bucketList = arrayListOf<Bucket>()
+    lateinit var dbHelper : DBHelper
+    lateinit var database : SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bucket)
 
+        val lvBucket = findViewById<ListView>(R.id.lv_bucket)
+        val bucketAdapter = BucketAdapter(this, bucketList)
+        for (i in 1.. 10) {
+            bucketList.add(Bucket("", 0))
+        }
+        lvBucket.adapter = bucketAdapter
 
+        dbHelper = DBHelper(this)
+        database = dbHelper.writableDatabase
+    }
+
+    override fun onBackPressed() {
+        for(bucket in bucketList){
+
+            Log.d("yyj", "con: " +  bucket.content + " // ch: " + bucket.checked)
+        }
+        //startActivity(Intent(this, MainActivity::class.java))
+        //finish()
     }
 }
