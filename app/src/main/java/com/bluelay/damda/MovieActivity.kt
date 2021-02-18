d84dbc74bcfd4a79f513423bd7ff73c759bb9084
winter28 <leegoeun1218@naver.com>
2021-02-18 20:36:33 +0900

    setting password

--- app/src/main/java/com/bluelay/damda/MovieActivity.kt
+++ app/src/main/java/com/bluelay/damda/MovieActivity.kt
@@ -1,6 +1,5 @@
 package com.bluelay.damda
 
-import android.app.Activity
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
 import android.content.ContentValues
@@ -8,19 +7,12 @@ import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.activity_movie.*
-import kotlinx.android.synthetic.main.activity_todo.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.*
 
