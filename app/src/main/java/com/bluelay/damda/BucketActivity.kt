6c3f67b126bda3d539d05994e0ab8149ec2a7e0d
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-02-24 02:58:06 +0900

    BucketActivity.kt

--- app/src/main/java/com/bluelay/damda/BucketActivity.kt
+++ app/src/main/java/com/bluelay/damda/BucketActivity.kt
@@ -3,16 +3,26 @@ package com.bluelay.damda
 import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_bucket.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.adapter_view_bucket.*
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 class BucketActivity : AppCompatActivity() {
     var bucketList = arrayListOf<Bucket>()
     lateinit var dbHelper : DBHelper
     lateinit var database : SQLiteDatabase
     var bid = -1
+    var color = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,26 +34,44 @@ class BucketActivity : AppCompatActivity() {
         val bucketAdapter = BucketAdapter(this, bucketList)
 
         //if (bid != -1) {  } else {}
+        //color =
         getBucketList()
         /*for (i in 1.. 10) {
-            bucketList.add(Bucket("", 0))
+            bucketList.add(Bucket("", 0, ""))
         }*/
         lvBucket.adapter = bucketAdapter
     }
 
     fun getBucketList() {
         bid = 1 //임시 bid
-        var columns = arrayOf(DBHelper.BUC_COL_ID, DBHelper.BUC_COL_DATE, DBHelper.BUC_COL_CHECKED, DBHelper.BUC_COL_CONTENT)
-        var selection = "bid=?"
+
+        var columns = arrayOf(DBHelper.BUCL_COL_ID, DBHelper.BUCL_COL_COLOR)
+        var selection = "_id=?"
         var selectArgs = arrayOf(bid.toString())
-        var c : Cursor = database.query(DBHelper.BUC_TABLE_NAME, columns, selection, selectArgs, null, null, null)
+        var c : Cursor = database.query(DBHelper.BUCL_TABLE_NAME, columns, selection, selectArgs, null, null, null)
+        c.moveToNext()
+        var view = findViewById<ConstraintLayout>(R.id.activity_bucket)
+        when (c.getInt(c.getColumnIndex(DBHelper.BUCL_COL_COLOR))) {
+            0 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            1 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_red))
+            2 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_yellow))
+            3 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_green))
+            4 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_blue))
+            5 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_purple))
+            6 -> view.setBackgroundColor(ContextCompat.getColor(this, R.color.pastel_pink))
+        }
+
+        columns = arrayOf(DBHelper.BUC_COL_ID, DBHelper.BUC_COL_DATE, DBHelper.BUC_COL_CHECKED, DBHelper.BUC_COL_CONTENT)
+        selection = "bid=?"
+        selectArgs = arrayOf(bid.toString())
+        c = database.query(DBHelper.BUC_TABLE_NAME, columns, selection, selectArgs, null, null, null)
         bucketList.clear()
         for (i in 1.. 10) {
             if (c.moveToNext()) {
-                bucketList.add(Bucket(c.getString(c.getColumnIndex(DBHelper.BUC_COL_CONTENT)), c.getInt(c.getColumnIndex(DBHelper.BUC_COL_CHECKED))))
+                bucketList.add(Bucket(c.getString(c.getColumnIndex(DBHelper.BUC_COL_CONTENT)), c.getInt(c.getColumnIndex(DBHelper.BUC_COL_CHECKED)), c.getString(c.getColumnIndex(DBHelper.BUC_COL_DATE))))
             }
             else {
-                bucketList.add(Bucket("", 0))
+                bucketList.add(Bucket("", 0, ""))
             }
         }
     }
@@ -52,7 +80,7 @@ class BucketActivity : AppCompatActivity() {
         Log.d("yyj", "BackPressed")
         var contentValues = ContentValues()
         contentValues.put(DBHelper.BUCL_COL_WDATE, System.currentTimeMillis())
-        contentValues.put(DBHelper.BUCL_COL_COLOR, 0)
+        //contentValues.put(DBHelper.BUCL_COL_COLOR, 4)
         contentValues.put(DBHelper.BUCL_COL_LOCK, 0)
 
         if (bid != -1) {
@@ -63,14 +91,15 @@ class BucketActivity : AppCompatActivity() {
             whereCluase = "bid=?"
             database.delete(DBHelper.BUC_TABLE_NAME, whereCluase, whereArgs)
         }
-        //else  {
-        // bid = database.insert(DBHelper.BUCL_TABLE_NAME, null, contentValues)
-        // }
+        else  {
+            bid = database.insert(DBHelper.BUCL_TABLE_NAME, null, contentValues).toInt()
+        }
+
         for(bucket in bucketList){
             contentValues.clear()
             if (bucket.content != "") {
                 contentValues.put(DBHelper.BUC_COL_BID, bid)
-                contentValues.put(DBHelper.BUC_COL_DATE, "2020.01.28")
+                contentValues.put(DBHelper.BUC_COL_DATE, bucket.date)
                 contentValues.put(DBHelper.BUC_COL_CHECKED, bucket.checked)
                 contentValues.put(DBHelper.BUC_COL_CONTENT, bucket.content)
                 database.insert(DBHelper.BUC_TABLE_NAME, null, contentValues)
